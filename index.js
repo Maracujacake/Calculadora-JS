4e3740b6d79fdff9ec7a608890c71f87b01dd6fb
Chris Maracujá <74564883+Maracujacake@users.noreply.github.com>
2021-08-03 13:17:50 -0300

    js

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 let n1 = document.querySelector('#n1')
 let n2 = document.querySelector('#n2')
 let resultado = document.querySelector('span')
-let btnsomar = document.querySelector('#btnsomar')
 
 function somar(){
     resultado.innerHTML = parseFloat(n1.value) + parseFloat(n2.value)
 }
 
 // usar o parseFloat para operações com números decimais!
-
 
 function subtrair(){
     resultado.innerHTML = parseInt(n1.value) - parseInt(n2.value)
@@ -22,3 +20,15 @@ function dividir(){
     resultado.innerHTML = parseInt(n1.value) / parseInt(n2.value)
 }
 
+/*
+function subtrair(){
+    resultado.innerHTML = parseFloat(n1.value) - parseFloat(n2.value)
+}
+
+function multiplicar(){
+    resultado.innerHTML = parseFloat(n1.value) * parseFloat(n2.value)
+}
+
+function dividir(){
+    resultado.innerHTML = parseFloat(n1.value) / parseFloat(n2.value)
+}
